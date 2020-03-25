dc9d9039cff12881bade7524761c102dc631e3b6
kn52 <ashish52922@gmail.com>
2020-03-25 16:44:57 +0530

    ADDED:Mockito in parking Lot

---
+++ src/test/java/com/bridgelabz/parking/lot/test/ParkingLotMockitoTest.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.parking.lot.test;
+
+import com.bridgelabz.parking.lot.DriverType;
+import com.bridgelabz.parking.lot.ParkingLot;
+import com.bridgelabz.parking.lot.ParkingLotStrategy;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ParkingLotMockitoTest {
+    ParkingLot parkingLot;
+    ParkingLotStrategy parkingLotStrategy;
+    Object vehicle;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLot=mock(ParkingLot.class);
+        parkingLotStrategy=mock(ParkingLotStrategy.class);
+        vehicle=new Object();
+    }
+
+    @Test
+    public void testMockito_On_getCapacity() {
+        when(parkingLot.getTotalCapacity()).thenReturn(10);
+        int n=parkingLot.getTotalCapacity();
+        Assert.assertEquals(10,n);
+    }
+
+    @Test
+    public void whengivenDriver_IsHandicap_ShouldReturn_One() {
+        ParkingLot parkingLot1=new ParkingLot();
+        parkingLot1.setCapacity(10);
+        Object vehicle1=new Object();
+        Object vehicle2=new Object();
+        parkingLot1.parkVehicle(vehicle1,DriverType.NORMAL,"asd");
+        parkingLot1.parkVehicle(vehicle2,DriverType.NORMAL,"asd");
+        when(parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP)).thenReturn(11);
+        int slot=parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP);
+        Assert.assertEquals(11,slot);
+    }
+}
