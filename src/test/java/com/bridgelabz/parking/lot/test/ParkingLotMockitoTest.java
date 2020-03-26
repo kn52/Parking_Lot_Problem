9eea9567412126a273810c8a974ccd5fd17ad4f3
kn52 <ashish52922@gmail.com>
2020-03-26 20:12:17 +0530

    ADDED:Mockito for parking vehicle

--- src/test/java/com/bridgelabz/parking/lot/test/ParkingLotMockitoTest.java
+++ src/test/java/com/bridgelabz/parking/lot/test/ParkingLotMockitoTest.java
@@ -2,13 +2,13 @@ package com.bridgelabz.parking.lot.test;
 
 import com.bridgelabz.parking.lot.DriverType;
 import com.bridgelabz.parking.lot.ParkingLot;
+import com.bridgelabz.parking.lot.ParkingLotException;
 import com.bridgelabz.parking.lot.ParkingLotStrategy;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ParkingLotMockitoTest {
     ParkingLot parkingLot;
@@ -24,13 +24,25 @@ public class ParkingLotMockitoTest {
 
     @Test
     public void testMockito_On_getCapacity() {
-        when(parkingLot.getTotalCapacity()).thenReturn(10);
-        int n = parkingLot.getTotalCapacity();
-        Assert.assertEquals(10, n);
+        try{
+            when(parkingLot.getTotalCapacity()).thenReturn(10);
+            int n = parkingLot.getTotalCapacity();
+            Assert.assertEquals(10, n);
+        }catch(ParkingLotException e){
+            e.printStackTrace();
+        }
+
     }
 
     @Test
-    public void name() {
+    public void whengivenVehicleParkedThow_Exception() {
+        try{
+            doThrow(new ParkingLotException("Parking is full", ParkingLotException.ExceptionType.NO_PARKING))
+                    .when(parkingLot).parkVehicle(any(),any(DriverType.class),anyString());
+            parkingLot.parkVehicle(new Object(),DriverType.NORMAL,"asd");
+        }catch (ParkingLotException e){
+            Assert.assertEquals("Parking is full",e.getMessage());
+        }
     }
 
     @Test
@@ -39,11 +51,15 @@ public class ParkingLotMockitoTest {
         parkingLot1.setCapacity(10);
         Object vehicle1=new Object();
         Object vehicle2=new Object();
-        parkingLot1.parkVehicle(vehicle1,DriverType.NORMAL,"asd");
-        parkingLot1.parkVehicle(vehicle2,DriverType.NORMAL,"asd");
-        when(parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP)).thenReturn(11);
-        int slot=parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP);
-        Assert.assertEquals(11,slot);
+        try{
+            parkingLot1.parkVehicle(vehicle1,DriverType.NORMAL,"asd");
+            parkingLot1.parkVehicle(vehicle2,DriverType.NORMAL,"asd");
+            when(parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP)).thenReturn(11);
+            int slot=parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP);
+            Assert.assertEquals(11,slot);
+        }catch (ParkingLotException e){
+            e.printStackTrace();
+        }
     }
 
 
@@ -53,20 +69,28 @@ public class ParkingLotMockitoTest {
         parkingLot1.setCapacity(10);
         Object vehicle1=new Object();
         Object vehicle2=new Object();
-        parkingLot1.parkVehicle(vehicle1,DriverType.NORMAL,"asd");
-        parkingLot1.parkVehicle(vehicle2,DriverType.NORMAL,"asd");
-        parkingLot1.setParkingStrategy(parkingLotStrategy);
-        when(parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP)).thenReturn(11);
-        int slot=parkingLot1.getSlotNumber(DriverType.HANDICAP, parkingLotStrategy);
-        Assert.assertEquals(11,slot);
+        try{
+            parkingLot1.parkVehicle(vehicle1,DriverType.NORMAL,"asd");
+            parkingLot1.parkVehicle(vehicle2,DriverType.NORMAL,"asd");
+            parkingLot1.setParkingStrategy(parkingLotStrategy);
+            when(parkingLotStrategy.getVehicleSlot(DriverType.HANDICAP)).thenReturn(11);
+            int slot=parkingLot1.getSlotNumber(DriverType.HANDICAP, parkingLotStrategy);
+            Assert.assertEquals(11,slot);
+        }catch (ParkingLotException e){
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void whenGettingTotal_Empty_Capacity_ShouldReturn_Wrong(){
         ParkingLot parkingLot1=new ParkingLot();
         parkingLot1.setCapacity(3);
-        when(parkingLot.getEmptySlots()).thenReturn(12);
-        int emptySlots=parkingLot.getEmptySlots();
-        Assert.assertEquals(12,emptySlots);
+        try{
+            when(parkingLot.getEmptySlots()).thenReturn(12);
+            int emptySlots=parkingLot.getEmptySlots();
+            Assert.assertEquals(12,emptySlots);
+        }catch(ParkingLotException e){
+            e.printStackTrace();
+        }
     }
 }
