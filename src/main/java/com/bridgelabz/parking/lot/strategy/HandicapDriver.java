8f346f01a08f945199be73826b30cab2e0a398c7
kn52 <ashish52922@gmail.com>
2020-04-01 00:59:49 +0530

    ADDED:Strategy for large vehicle

--- src/main/java/com/bridgelabz/parking/lot/strategy/HandicapDriver.java
+++ src/main/java/com/bridgelabz/parking/lot/strategy/HandicapDriver.java
@@ -9,7 +9,8 @@ public class HandicapDriver implements IParkingStrategy {
 
     @Override
     public ParkingLot getParkingLot(List<ParkingLot> parkingLots){
-        return parkingLots.stream().filter(lots->lots.getEmptySlots()>0).findFirst()
+        return parkingLots.stream().filter(lots->lots.getEmptySlots()>0)
+                .findFirst()
                 .orElseThrow(()-> new ParkingLotException("No parking lot found", ParkingLotException.ExceptionType.NO_PARKING_LOT_AVAILABLE));
     }
 }
--- src/main/java/com/bridgelabz/parking/lot/strategy/LargeVehicleDriver.java
+++ src/main/java/com/bridgelabz/parking/lot/strategy/LargeVehicleDriver.java
@@ -1,12 +1,17 @@
 package com.bridgelabz.parking.lot.strategy;
 
 import com.bridgelabz.parking.lot.details.ParkingLot;
+import com.bridgelabz.parking.lot.exception.ParkingLotException;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class LargeVehicleDriver implements IParkingStrategy {
     @Override
     public ParkingLot getParkingLot(List<ParkingLot> parkingLots) {
-        return null;
+        return parkingLots.stream().filter(lots->lots.getEmptySlots()>0)
+                .sorted(Comparator.comparing(lots->lots.getEmptySlots(),Comparator.reverseOrder()))
+                .findFirst()
+                .orElseThrow(()-> new ParkingLotException("No parking lot found", ParkingLotException.ExceptionType.NO_PARKING_LOT_AVAILABLE));
     }
 }
